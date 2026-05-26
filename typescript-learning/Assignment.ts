fdf9b04a17ade6155a8aee7c4b2afc2cc685cc00
Darshan Natraj <darshan.natraj08@gmail.com>
2026-05-26 20:39:28 +0530

    Added Assignment file

---
+++ typescript-learning/Assignment.ts
@@ -0,0 +1,86 @@
+//1. Temperature of a city in degrees Celsius: 25.5
+let temperature:number = 25.5
+console.log(`Temperature of a city in degrees Celsius: ${temperature}`)
+
+//2. Whether a customer has placed an order: true or false
+let customer:boolean = true;
+let order:boolean = false;
+console.log(`Whether a customer has placed an order: ${customer} or ${order}`)
+
+//3. Person's phone number: "123-456-7890"
+let phone:string = "123-456-7890";
+console.log(`Person's phone number: ${phone}`)
+
+//4. Amount of money in a customer's bank account: 1000.50
+let money:number =  1000.50
+console.log(`Amount of money in a customer's bank account: ${money}`)
+
+//5. Person's email address: "john.doe@example.com"
+let email:string = "john.doe@example.com"
+console.log(`Person's email address: ${email}`)
+
+//6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+let latitude:number = 37.7749
+let longitude:number = -122.4194
+
+console.log(`Coordinates of a location (latitude, longitude): ${latitude}, ${longitude}`)
+
+//7. Person's marital status: true or false
+let X:boolean = true
+let Y:Boolean = false
+console.log(`Person's marital status: ${X} or ${Y}`)
+
+//8.Person's occupation: "Software Engineer"
+let occupation:string = "Software Engineer"
+console.log(`Person's occupation: ${occupation}`)
+
+//9.Person's favourite colour: "Blue"
+let color:string = "Blue"
+console.log(`Person's favourite colour: ${color}`)
+
+//10.Current year: 2023
+let year:number = 2023
+console.log(`Current year: ${year}`)
+
+//11.Number of followers on a social media platform: 1,000,000
+const followers: number = 1000000;
+console.log(`Number of followers on a social media platform: ${followers.toLocaleString('en-US')}`);
+
+//12.Rating of a movie: 7.5
+let rating:number = 7.5
+console.log(`Rating of a movie: ${rating}`)
+
+//13.Person's blood type: 'A'
+let blood:string = 'A'
+console.log(`Person's blood type: ${blood}`)
+
+//14.Title of a book: "To Kill a Mockingbird"
+let title:string = "To Kill a Mockingbird"
+console.log(`Title of a book: ${title}`)
+
+//15.Number of employees in a company: 500
+let employee:number = 500
+console.log(`Number of employees in a company: ${employee}`)
+
+//16.Time of an event: 2:30 PM
+//const eventTime: Date = new Date();
+let time:string = "2:30 PM"
+//eventTime.setHours(14, 30);
+//console.log('${time} eventTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })')
+console.log(`Time of an event: ${time}`)
+
+//17.Name of a country: "United States"
+let country:string = "United States"
+console.log(`Name of a country: ${country}`)
+
+//18.Person's eye color: "Brown"
+let colour:string = "Brown"
+console.log(`Person's eye color: ${colour}`)
+
+//19.Person's birthplace: "New York City"
+let place:string = "New York City"
+console.log(`Person's birthplace: ${place}`)
+
+//20. Distance between two cities: 200.5
+let dist:number = 200.5
+console.log(`Distance between two cities: ${dist}`)
